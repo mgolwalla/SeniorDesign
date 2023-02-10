d326356d10a452a932a44ff2e8be5c4efe1444e9
Mariam Golwalla <mgolw001@ucr.edu>
2023-02-10 15:49:25 -0800

    no errors, _app.js has all code commented

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// export default function App({ Component, pageProps }) {
+// var SerialPort = require("serialport");
+// const parsers = SerialPort.parsers;
+// const parser = new parsers.Readline({
+//   delimiter: "\r\n",
+// });
+
+// var port = new SerialPort("COM3 (Arduino Uno)", {
+//   baudRate: 9600,
+//   dataBits: 8,
+//   parity: "none",
+//   stopBits: 1,
+//   flowControl: false,
+// });
+
+// port.pipe(parser);
+// parser.on("data", function (data) {
+//   console.log(data);
+// });
+//   return <Component {...pageProps} />;
+// }
+
+import React from "react";
+
+const App = ({ Component }) => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
