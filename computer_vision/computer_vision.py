ceea7bb77405edbf395ac768271ccfd0dfd7abba
mgolwalla <mgolw001@ucr.edu>
2023-03-08 14:25:46 -0800

    inital test

---
+++ computer_vision/computer_vision.py
@@ -0,0 +1,60 @@
+import cv2
+from picamera2 import Picamera2
+
+
+#thres = 0.45 # Threshold to detect object
+
+classNames = []
+classFile = "/home/pi/Desktop/Object_Detection_Files/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/pi/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+    #print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    # objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                # objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+    return img
+
+
+if __name__ == "__main__":
+
+    # cap = cv2.VideoCapture(-2)
+    # cap.set(3,640)
+    # cap.set(4,480)
+    # #cap.set(10,70)
+    cv2.startWindowThread()
+
+    picam2 = Picamera2()
+    picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam2.start()
+
+
+    while True:
+        img = picam2.capture_array()
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
+        result = getObjects(img,0.45,0.2, objects = ["potted plant"])
+        cv2.imshow("Output",img)
+        cv2.waitKey(1)
